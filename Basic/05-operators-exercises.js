42fc4d6e60e1199a98b7b222ba0c95b1ea5f3870
Sandra Floro <florosandra@gmail.com>
2026-07-18 19:22:23 +0100

    05-operator-exercises - done2

--- Basic/05-operators-exercises.js
+++ Basic/05-operators-exercises.js
@@ -5,118 +5,74 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=6458
 
 // 1. Crea una variable para cada operación aritmética
 
-let x = 4
-let y = 2
+let a = 8
+let b = 4
 
-console.log(x + y) // Suma
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(a % b)
+console.log(a ** b)
 
-console.log(x - y) // Resta
-
-console.log (x * y) // Multiplicación
-
-console.log (x / y) // División
-
-console.log (x % y) // Módulo
-
-console.log (x ** y) // Exponente
-
-x++
-console.log(x) // Incremento
-
-y--
-console.log(y) // Decremento
+a++
+console.log(a)
+b--
+console.log(b)
 
 
 // 2. Crea una variable para cada tipo de operación de asignación, que haga uso de las variables utilizadas para las operaciones aritméticas
 
-let miVaina = 1
-console.log(miVaina) // Creación de nueva variable
+let c = 5
+let d = 10
 
-miVaina += x
-console.log(miVaina) // Sumar y asignar
-
-x -= y
-console.log(x) // Restar y asignar
-
-x *= y
-console.log(x) // Multiplicación y asignar
-
-x /= y
-console.log(x) // División y asignar
-
-x %= y
-console.log(x) // Módulo y asignar
-
-miVaina **= x
-console.log(miVaina) // Exponente y asignar
+console.log(c += b)
+console.log(d -= a)
+console.log(c *= a)
+console.log(c /= a)
+console.log(a %= b)
+console.log(b **= a)
 
 
 // 3. Imprime 5 comparaciones verdaderas con diferentes operadores de comparación
 
-let a = 8
-let b = 20
-
-console.log(a < b) // Menor que
-
-console.log(a >= 8) // Mayor o igual que
-
-console.log(b == "20") // Igualdad por valor
-
-console.log(b === 20) // Igualdad estricta
-
-console.log(a != b) // Desigualdad por valor
+console.log(5 < 10)
+console.log(a === 0)
+console.log(30 > b)
+console.log(40 >= 40)
+console.log(1 == "1")
 
 
 // 4. Imprime 5 comparaciones falsas con diferentes operadores de comparación
 
-console.log(a > b) // Mayor que
-
-console.log(a >= b) // Mayor o igual que
-
-console.log(a == b) // Igualdad por valor
-
-console.log(a === b) // Igualdad estricta
-
-console.log (a !== 8) // Desigualdad estricta
-
+console.log(10 < 5)
+console.log(a !== 0)
+console.log(b > 30)
+console.log(39 >= 40)
+console.log(1 != "1")
 
 // 5. Utiliza el operador lógico and
 
-console.log(a > 9 && 8 < 10) // false
-
-console.log (a < 11 && b > 3 && 90 == "90") // true
-
+console.log(10 > 5 && 1 == "1")
 
 // 6. Utiliza el operador lógico or
 
-console.log (a == 8 || 20 === "20") // true
-
-console.log (a > 10 || 9 < 3 || 8 < 1) // false
-
+console.log(10 < 5 || 6 === 6)
 
 // 7. Combina ambos operadores lógicos
 
-console.log(10 > 5 && 70 < 4 || 20 == b) // true
-console.log(10 > 5 || 70 < 4 && 20 == b) // true -- && se ejecuta primero antes que || da igual el orden
+console.log((10 < 5 && 7 === 7) || 5 === "5")
 
 // 8. Añade alguna negación
 
-console.log(80 != b && b == 20 || a === "8") // true
-
-console.log (!(7 > 2 && b == 20) || a == "10") // false
-
-console.log (!(7 > 2 && b == 20) || a == "8") // true
+console.log(!(10 > 5 && 9 == "9") || 5 == "5")
 
 // 9. Utiliza el operador ternario
 
-const isSunny = true
-isSunny ? console.log("It is sunny today") : console.log("It is not sunny today")
+const isNight = false
+
+isNight ? console.log("Yes, it is nighttime") : console.log("No, it's not nighttime yet.")
 
 // 10. Combina operadores aritméticos, de comparáción y lógicas
 
-let c = 10
-let d = 5
-
-console.log((c + 9) < 3 && d === "5" || 90 > 55 || !(5 === d)) // true
-
-console.log((c + 9) < 3 && d === "5" || 90 < 55 || !(5 === d)) // false
+console.log(!(9 + 9 > 80 && 8 == "8") || 4 * 5 !== 20)
